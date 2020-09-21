003ccc42bbde4acb438f3eae1cf3a040cc341502
Anggit <anggitpp23@gmail.com>
2020-09-21 10:48:41 +0700

    upload project vue

---
+++ src/components/WomanShayna.vue
@@ -0,0 +1,134 @@
+<template>
+    <!-- Women Banner Section Begin -->
+    <section class="women-banner spad">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 mt-5">
+                    <carousel class="product-slider" :items = '3' :nav="false" :dots="false" :autoplay="true">
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/door lock.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><router-link to="/product">+ Quick View</router-link></li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Security</div>
+                                <router-link to="/product"><a href="#">
+                                    <h5>Door Lock</h5>
+                                </a></router-link>
+                                <div class="product-price">
+                                    $14.00
+                                    <span>$35.00</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/cctv indoor.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><router-link to="/product">+ Quick View</router-link></li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Security</div>
+                                <a href="#">
+                                    <h5>CCTV Indoor</h5>
+                                </a>
+                                <div class="product-price">
+                                    $14.00
+                                    <span>$35.00</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/cctv outdoor.png" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                   <li class="quick-view"><router-link to="/product">+ Quick View</router-link></li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Security</div>
+                                <a href="#">
+                                    <h5>CCTV Outdoor</h5>
+                                </a>
+                                <div class="product-price">
+                                    $13.00
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/ptz indoor.png" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><router-link to="/product">+ Quick View</router-link></li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Security</div>
+                                <a href="#">
+                                    <h5>CCTV PTZ Indoor</h5>
+                                </a>
+                                <div class="product-price">
+                                    $34.00
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/smart breaker.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><router-link to="/product">+ Quick View</router-link></li>
+                                    <li class="w-icon">
+                                        <a href="#"><i class="fa fa-random"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Electronic</div>
+                                <a href="#">
+                                    <h5>Smart Breaker</h5>
+                                </a>
+                                <div class="product-price">
+                                    $34.00
+                                </div>
+                            </div>
+                        </div>
+                    </carousel>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Women Banner Section End -->
+</template>
+<script>
+import carousel from 'vue-owl-carousel'
+
+export default {
+    name: 'WomanShayna',
+    components:{
+    carousel
+    }
+}
+</script>
+<style scoped>
+.product-item{
+    margin-right: 25px;
+}
+</style>
